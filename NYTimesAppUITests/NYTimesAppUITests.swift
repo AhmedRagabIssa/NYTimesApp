5f9aee15a0f3ee296270de41d28f6d21acf4df7d
Ahmed Ragab <a.ragab@ibswe.com>
2019-07-10 04:55:15 +0200

    - Writing the UITests, - Running the UITests.

--- NYTimesAppUITests/NYTimesAppUITests.swift
+++ NYTimesAppUITests/NYTimesAppUITests.swift
@@ -10,6 +10,8 @@ import XCTest
 
 class NYTimesAppUITests: XCTestCase {
 
+    let app = XCUIApplication()
+    
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
 
@@ -26,9 +28,30 @@ class NYTimesAppUITests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testNavigationFromListingToDetails() {
+        
+        app.tables/*@START_MENU_TOKEN@*/.staticTexts["10 Findings That Contradict Medical Wisdom. Doctors, Take Note."]/*[[".cells.staticTexts[\"10 Findings That Contradict Medical Wisdom. Doctors, Take Note.\"]",".staticTexts[\"10 Findings That Contradict Medical Wisdom. Doctors, Take Note.\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        
+        let elementsQuery = app.scrollViews.otherElements
+        elementsQuery.scrollViews.children(matching: .other).element.swipeLeft()
+        elementsQuery.children(matching: .scrollView).element.swipeRight()
     }
-
+    
+    func testArticlesDetailsDateLabelExist() {
+        
+        app.tables/*@START_MENU_TOKEN@*/.staticTexts["10 Findings That Contradict Medical Wisdom. Doctors, Take Note."]/*[[".cells.staticTexts[\"10 Findings That Contradict Medical Wisdom. Doctors, Take Note.\"]",".staticTexts[\"10 Findings That Contradict Medical Wisdom. Doctors, Take Note.\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        
+        let elementsQuery = app.scrollViews.otherElements
+        let dateLabel = elementsQuery.staticTexts["2019-07-01"]
+        XCTAssert(dateLabel.exists, "The date label dose not exist.")
+    }
+    
+    func testBackFromDetailsToListing() {
+        app.tables/*@START_MENU_TOKEN@*/.staticTexts["10 Findings That Contradict Medical Wisdom. Doctors, Take Note."]/*[[".cells.staticTexts[\"10 Findings That Contradict Medical Wisdom. Doctors, Take Note.\"]",".staticTexts[\"10 Findings That Contradict Medical Wisdom. Doctors, Take Note.\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        let nytimesArticlesNavigationBar = app.navigationBars["NYTimes Articles"]
+        nytimesArticlesNavigationBar.buttons["NYTimes Articles"].tap()
+        let nav = nytimesArticlesNavigationBar.otherElements["NYTimes Articles"]
+        XCTAssert(nav.exists, "The Listing navigation bar dose not exist.")
+    }
+    
 }
